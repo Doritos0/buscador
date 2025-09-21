1407c111e93bfb8c1ae534ad8c14acdf3cbbf144
Doritos0 <meyesblue@gmail.com>
2025-09-21 00:25:36 -0300

    uso de useRef, useMemo, useCallback buscado de peliculas (importante el debounce)

--- src/guias/mapeo_peliculas.js
+++
@@ -1,8 +0,0 @@
-export function mappedPeliculas(peliculas) {
-    return peliculas?.Search?.map(pelicula => ({
-        id: pelicula.imdbID,
-        title: pelicula.Title,
-        year: pelicula.Year,
-        poster: pelicula.Poster
-    }))
-}
